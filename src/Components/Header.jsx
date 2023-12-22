8af1bc730e57eaf2327805a17e3b8cc04e530215
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-22 12:16:34 +0530

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,32 @@ function Header() {
   return (
     <div>
       <h1>Company's Budget Allocation</h1>
+
+      <div style={{ display: "flex" }}>
+        <div>
+          <p>
+            Budget: $<input></input>
+          </p>
+        </div>
+        <div>
+          <p>
+            Remaining: <input></input>
+          </p>
+        </div>{" "}
+        <div>
+          <p>
+            Spent so far: <input></input>
+          </p>
+        </div>{" "}
+        <div>
+          <select>
+            <option>$ Dollar</option>
+            <option>£ Pound</option>
+            <option>€ Euro</option>
+            <option>₹ Ruppee</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
