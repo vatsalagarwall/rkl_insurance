f4efa80ed4a7050b3a7a15859aa7f212f2105f9f
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-22 12:29:35 +0530

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [selectedCurrency, setSelectedCurrency] = useState("$");
+
+  const handleCurrencyChange = (event) => {
+    setSelectedCurrency(event.target.value);
+  };
+
   return (
     <div>
       <h1>Company's Budget Allocation</h1>
 
-      <div style={{ display: "flex" }}>
-        <div>
-          <p>
-            Budget: $<input></input>
-          </p>
-        </div>
-        <div>
-          <p>
-            Remaining: <input></input>
-          </p>
-        </div>{" "}
-        <div>
-          <p>
-            Spent so far: <input></input>
-          </p>
-        </div>{" "}
-        <div>
-          <select>
-            <option>$ Dollar</option>
-            <option>£ Pound</option>
-            <option>€ Euro</option>
-            <option>₹ Ruppee</option>
-          </select>
+      <div class="container text-center" style={{ marginTop: "30px" }}>
+        <div class="row align-items-start">
+          <div class="col" style={{ backgroundColor: "grey" }}>
+            <p>
+              Budget: {selectedCurrency}
+              <input></input>
+            </p>
+          </div>
+          <div class="col" style={{ backgroundColor: "#D1FFBD" }}>
+            <p>Remaining: {selectedCurrency}</p>
+          </div>
+          <div class="col" style={{ backgroundColor: "#ADD8E6" }}>
+            <p>Spent so far: {selectedCurrency}</p>
+          </div>
+          <div class="col" style={{ backgroundColor: "#98FB98" }}>
+            <select onChange={handleCurrencyChange}>
+              <option value="$">Dollar</option>
+              <option value="£">Pound</option>
+              <option value="€">Euro</option>
+              <option value="₹">Rupee</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
